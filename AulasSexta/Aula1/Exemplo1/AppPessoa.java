f346045fe4766358f1a6609c64672e7312e02890
Marcelo Bindi Leal <mbindileal10@gmail.com>
2023-03-03 20:21:01 -0300

    Exemplo01 Aula01

---
+++ AulasSexta/Aula1/Exemplo1/AppPessoa.java
@@ -0,0 +1,11 @@
+package AulasSexta.Aula1.Exemplo1;
+
+public class AppPessoa{
+    public static void main(String[] args) {
+        
+        Pessoa p = new Pessoa("Marcelo");
+
+        p.apresentar();
+
+    }
+}
